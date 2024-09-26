af44582b772bd9f860b776ff886308a0441e443e
Deepak <mail.deepak3105@gmail.com>
2024-09-27 00:21:39 +0530

    added DSA with c++

---
+++ if-else.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+/*int main(){
+
+    int a,b;
+
+    cout<< "Enter the value of a " << endl;
+    cin>>a;
+
+    cout<< "Enter the value of b " << endl;
+    cin>>b;
+
+    if(a<b){
+        cout<< "b is greater than a " << endl;
+    }
+        if(a>b){
+            cout<< "a is greater than b " << endl;
+        }
+}*/
+
+int main(){
+
+    int a = 9;
+    if(a==9){
+        cout<< "NINY" << endl;
+    }
+
+    if(a>0){
+        cout<< "POSITIVE";
+    }
+
+    else{
+        cout<< "NEGETIVE";
+    }
+}
