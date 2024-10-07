e7b1146168212eaa4ea14f8b371bd55a76786a1f
Deepak <mail.deepak3105@gmail.com>
2024-10-07 08:53:16 +0530

    added BitwiseOperator

---
+++ BitwiseOperator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 4;
+    int b = 6;
+
+cout << "~a" << ~a << endl;
+cout << "a&b" << (a&b) << endl;
+cout << "a|b" << (a|b) << endl;
+cout << "a^b" << (a^b) << endl;
+
+}
+
+/*OUTPUT:-
+
+
+~a -5
+a&b 4
+a|b 6
+a^b 2
+
+*/
